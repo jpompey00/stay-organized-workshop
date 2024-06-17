7fcb23a63ba14b3f4824a5bd070380f9604484ec
Julian Pompey <jupompey00@gmail.com>
2024-06-17 14:38:32 -0400

    javascript checkpoint 3

--- scripts/add-new-user.js
+++ scripts/add-new-user.js
@@ -12,13 +12,11 @@ const API_URL = "http://localhost:8083/api/users";
 window.onload = () => {
     console.log("connected");
 
-    addUser();
     submitButton.onclick = onsubmitButtonClick;
 }
 
 function onsubmitButtonClick() {
-    let username;
-    let password;
+
     //check if the inputboxes all have values
     if (usernameInputBox.value != ""
         && passwordInputBox.value != ""
@@ -29,10 +27,8 @@ function onsubmitButtonClick() {
             outputTextBox.innerHTML = "Passwords do not match"
         } else {
             //what runs if all the validation is correct.
-            username = usernameInputBox.value;
-            password = passwordInputBox.value;
-
-
+            let userToAdd = createUserJson(nameInputBox.value, usernameInputBox.value,passwordInputBox.value);
+            addUser(userToAdd);
         }
     } else {
         outputTextBox.innerHTML = "Please fill out the entire form"
@@ -44,24 +40,36 @@ function onsubmitButtonClick() {
 
 
 function addUser(userToAdd){
+    console.log(userToAdd);
+
     fetch(API_URL, {
         method: "POST",
-        body: userToAdd,
-        headers: {"Content-type":"application/json; charset=UTF-8"}
+        body: JSON.stringify(userToAdd),
+        headers: {"Content-type":
+        "application/json; charset=UTF-8"}
     })
-    .then(response => response.json())
+    .then(response => 
+        {   console.log(response);
+          //  alert(response);
+            return response.json()
+        }
+    )
     .then(data => {
         console.log(data)
-    })
-    .catch() //add code here for 403 Error I think 403 returns sucessfully.
+
+        return false;
+    });
+    //.catch() //add code here for 403 Error I think 403 returns sucessfully.
+
 }
 
 
 //add some algorithm to simulate a random password and a way to test that.
 function createUserJson(name,username,password){
     let userObject = {
-        "name" : name,
-        "username" : username,
-        "password" : password
+        "name" : `${name}`,
+        "username" : `${username}`,
+        "password" : `${password}`
     }
+    return userObject;
 }
